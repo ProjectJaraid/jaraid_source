81b05bfe8097346caed2ebc825655896605b84ed
Hugh Cayless <philomousos@gmail.com>
2020-12-16 21:30:16 -0500

    Updating make-CETEIcean action.

--- .github/actions/pre-process-ceteicean/process.sh
+++ .github/actions/pre-process-ceteicean/process.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for f in `ls tei/jaraid_master*.xml`
-do
-  echo "Pre-process master file for publication with CETEIcean and save as HTML"
-  saxon -s:"$f" -xsl:xslt/make-CETEIcean-3.xsl
-done
+
+git config --global user.email "$ACTION_USER_EMAIL"
+git config --global user.name "$ACTION_USER_NAME"
+git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/ProjectJaraid/projectjaraid.github.io.git" /opt/projectjaraid
+
+echo "Pre-process master file for publication with CETEIcean and save as HTML"
+saxon -s:tei/jaraid_master.TEIP5.xml -xsl:xslt/make-CETEIcean.xsl outfile=chrono
+saxon -s:jaraid_master.TEIP5.xml -xsl:xslt/make-CETEIcean.xsl outfile=fihris
+
+cd /opt/projectjaraid
+git commit -am "Update from jaraid_source, commit $GITHUB_SHA."
+git push origin master
