ede243756bbf79042c662fae0839730b7e2b5bc4
Till Grallert <till.grallert@fu-berlin.de>
2020-12-23 11:29:04 +0100

    improved action to no apply to anything but the master file

--- .github/actions/update-arabic-columns/process.sh
+++ .github/actions/update-arabic-columns/process.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-for f in `ls tei/jaraid_master*.xml`
-do
-  echo "Update the Arabic columns in the master with data from the authority file"
-  saxon -s:"$f" -xsl:xslt/tei_add-arabic-columns.xsl -o:"$f" p_id-editor='github'
-done
+
+echo "Update the Arabic columns in the master with data from the authority file"
+saxon -s:tei/jaraid_master.TEIP5.xml -xsl:xslt/tei_add-arabic-columns.xsl -o:tei/jaraid_master.TEIP5.xml p_id-editor='github'
