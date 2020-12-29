d9c66976721068adf67a4d53e85b861fec4b8baa
Till Grallert <till.grallert@fu-berlin.de>
2020-12-29 12:41:36 +0100

    deleted all files not strictly part of the dataset

--- .github/actions/update-arabic-columns/process.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Update the Arabic columns in the master with data from the authority file"
-saxon -s:tei/jaraid_master.TEIP5.xml -xsl:https://projectjaraid.github.io/jaraid_tools/xslt/tei_add-arabic-columns.xsl -o:tei/jaraid_master.TEIP5.xml p_id-editor='github'
